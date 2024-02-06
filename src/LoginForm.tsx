40a8ad15a6ee62fc650d2610f0b8197f49106421
DESKTOP-7B3O0B3\ichar <richardchiqui3@yahoo.com>
2024-02-05 21:53:34 -0500

    setup login page, missing forgot password option

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -6,7 +6,25 @@ export default function LoginForm(){
 
     return(
          <div className='LoginForm'>
-            <h1>Login</h1>
+            <div className='loginForm-container'>
+                <h1 className='login-title'>Login</h1>
+                <form className='login-form'>  
+                    <label htmlFor="Username"></label>
+                    <input type="text" id="Username" name="Username" placeholder="Username" className='login-input'></input>
+
+                    <label htmlFor="Password"></label>
+                    <input type="text" id="Password" name="Password" placeholder="Password" className='login-input'></input>
+
+
+                </form>
+                <button className='login-btn'>Login</button>
+                <h6 className='create-account-text'>Don't have an account? <strong className='signupText'>Sign Up!</strong></h6>
+             
+            </div>
+
+            
+
+            
         
         </div>
     )
