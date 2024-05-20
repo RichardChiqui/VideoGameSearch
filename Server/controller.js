4da295ec341a2f023d5f070e2c64b05473e528af
DESKTOP-7B3O0B3\ichar <richardchiqui3@yahoo.com>
2024-05-20 18:15:29 -0400

    using quelize

--- Server/controller.js
+++ Server/controller.js
@@ -1,21 +1,36 @@
 const pool = require("./db");
 const queries = require("./queries");
 
-const validateUser = (req,res) => {
-    const {username, password } = req.body
-    console.log("I am in controller ");
-    pool.query(queries.validateUser, [username,password], (error, results) =>{
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    })
+const {User} = require('./Models/UserModel');
+
+const validateUser = async (req,res) => {
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return res.status(400).json({ error: 'No user found with this email' });
+      }
+  
+      const isMatch = await password === user.password;
+      if (!isMatch) {
+        return res.status(400).json({ error: 'Incorrect password' });
+      }
+  
+    //   const token = jwt.sign({ id: user.id }, 'your_jwt_secret', { expiresIn: '1h' });
+    //   res.json({ token });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
 }
 
-const addUser = (req,res) =>{
-    const {username, password, email} = req.body
-    pool.query(queries.addUser, [username,password,email], (error, results) =>{
-        if (error) throw error;
-        res.status(200).json(results.rows);
-    })
+const addUser = async (req,res) =>{
+    const { username, email, password } = req.body;
+    try {
+        const user = await User.create({ username, email, password });
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 }
 
 
--- Server/db.js
+++ Server/db.js
@@ -1,11 +1,19 @@
-const Pool  = require('pg').Pool;
+// const Pool  = require('pg').Pool;
 
-const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'videoGame',
-    password:'test123',
-    port:'5432'
-})
+// const pool = new Pool({
+//     user:'postgres',
+//     host:'localhost',
+//     database:'videoGame',
+//     password:'test123',
+//     port:'5432'
+// })
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('videoGame', 'postgres', 'test123', {
+  host: 'localhost',
+  dialect: 'postgres'  // Choose one: 'mysql' | 'mariadb' | 'postgres' | 'mssql' | 'sqlite'
+});
 
-module.exports = pool;
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
+//module.exports = pool;
